3f32820a24d0331a6b0e6522a47d2d3598822de7
mac-outlier <mattc@outlier.org>
2021-08-16 16:07:21 -0400

    Update console msg

--- fetch-db.js
+++ fetch-db.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 const request = require('request')
 request('https://outlier-coding-test-data.netlify.app/students.db').pipe(fs.createWriteStream('students.db'))
-console.log('Successfully fetched students.db.')
+console.log('Fetching students.db...')
