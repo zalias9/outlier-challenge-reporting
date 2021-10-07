8f8a25219c5306c0fa0f5dcbd2b27df3447e85a7
unknown <zaidahmed@gmail.com>
2021-10-08 00:15:51 +0400

    * Implement getStudent method in api.js * Add tests for /student/:id endpoint in test.js

--- api.js
+++ api.js
@@ -18,7 +18,19 @@ async function getHealth (req, res, next) {
 }
 
 async function getStudent (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+  try {
+    const student = await knex('students').where('id', req.params.id).first()
+    if (student) {
+      // Assuming you don't want to send password_hash with the object
+      delete student.password_hash
+      res.status(200).json(student).send()
+    } else {
+      res.status(404).json({"error" : "Student ID doesn't exist"}).send()
+    }
+  } catch (e) {
+    console.log(e)
+    res.status(500).end()
+  }
 }
 
 async function getStudentGradesReport (req, res, next) {
--- test.js
+++ test.js
@@ -20,6 +20,45 @@ tape('health', async function (t) {
   }
 })
 
+tape('GET /student/1 [Student id exists]', async function (t) {
+  const url = `${endpoint}/student/1`
+  const student1 = {
+    "id": 1,
+    "first_name": "Scotty",
+    "last_name": "Quigley",
+    "email": "Scotty79@hotmail.com",
+    "is_registered": 1,
+    "is_approved": 1,
+    "address": "241 Denesik Knolls Apt. 955",
+    "city": "Buffalo",
+    "state": "ME",
+    "zip": "04710",
+    "phone": "1-503-560-6954",
+    "created": "1628767983203.0",
+    "last_login": "1628770445749.0",
+    "ip_address": "2.137.18.155"
+  }
+  try {
+    const { data, response } = await jsonist.get(url);
+    t.ok(response.statusCode === 200, 'Should have reponse code 200')
+    t.deepEqual(data, student1, 'Should return Student ID 1 info')
+  } catch (e) {
+    t.error(e)
+  }
+})
+
+tape('GET /student/0 [Student id does not exist]', async function (t) {
+  const url = `${endpoint}/student/0`
+  const student0 = { "error" : "Student ID doesn't exist" }
+  try {
+    const { data, response } = await jsonist.get(url);
+    t.ok(response.statusCode === 404, 'Should have reponse code 404')
+    t.deepEqual(data, student0, 'Should return object with error')
+  } catch (e) {
+    t.error(e)
+  }
+})
+
 tape('cleanup', function (t) {
   server.closeDB()
   server.close()
